aff7aa10924137c5a77a4611a4d70fd9a3288d17
NayakaZNA <darkelytra@gmail.com>
2024-10-17 14:51:37 +0700

    docs: change comment

--- src/txtIO.java
+++ src/txtIO.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -29,7 +30,7 @@ public class txtIO {
                 return inputMatrix;
                 
         } catch (IOException e) {
-            // Jika file kosong, kembalikan null
+            // Jika file kosong atau tidak ada, kembalikan null
             return null;
         }
 
